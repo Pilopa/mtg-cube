d65b616b8e8c94eef3fca1374363c0768b3ce075
Konstantin Schaper <Konstantin.Schaper@trackback.net>
2019-02-11 21:06:43 +0000

    feat: update layout and theming

--- src/app/shared/components/card-grid/card-grid.component.ts
+++ src/app/shared/components/card-grid/card-grid.component.ts
@@ -1,5 +1,10 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { CubeCardModel } from '@app/shared/models/firestore/cubes/cube.model';
+import { Select } from '@ngxs/store';
+import { LayoutState } from '@app/shared/state/layout/layout.state';
+import { Observable } from 'rxjs';
+import { LayoutSize } from '@app/shared/models/layout-size.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-card-grid',
@@ -9,9 +14,51 @@ import { CubeCardModel } from '@app/shared/models/firestore/cubes/cube.model';
 })
 export class CardGridComponent {
 
-  @Input() cards: CubeCardModel[];
+  @Input() cards: CubeCardModel[] = (() => {
+    const result: CubeCardModel[] = [];
+
+    for (let i = 0; i < 150; i++) {
+      result.push({
+        id: `C${i}`
+      });
+    }
+
+    return result;
+  })();
+
+  @Select(LayoutState.getLayoutSize)
+  layoutSize$: Observable<LayoutSize>;
+
+  readonly columnCount$ = this.layoutSize$.pipe(
+    map(size => {
+      switch (size) {
+        case LayoutSize.SMALL:
+          return 3;
+        case LayoutSize.MEDIUM:
+          return 6;
+        case LayoutSize.LARGE:
+          return 8;
+        default:
+          return 6;
+      }
+    })
+  );
+
+  readonly gutterSize$ = this.layoutSize$.pipe(
+    map(size => {
+      switch (size) {
+        case LayoutSize.SMALL:
+          return 1;
+        case LayoutSize.MEDIUM:
+          return 2;
+        case LayoutSize.LARGE:
+          return 4;
+        default:
+          return 2;
+      }
+    })
+  );
 
   constructor() { }
-
 
 }

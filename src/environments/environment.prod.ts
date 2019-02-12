079343d91e632535b41da972ee831d8ca835fe32
Konstantin Schaper <Konstantin.Schaper@trackback.net>
2019-02-12 18:15:39 +0000

    chore(config): update production firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
   indexVersionHash: '91d56d60371a0984f6064b0f067c2f209082cbce',
   cardVersionHash: '66a90e1c9a34b96621face84cc26e655cc3e7945',
   firebase: {
-    apiKey: 'AIzaSyD3B5wKdnoB0yshJxbQv-_JFFNEEKiSpPk',
-    authDomain: 'mtg-cube-dev.firebaseapp.com',
-    databaseURL: 'https://mtg-cube-dev.firebaseio.com',
-    projectId: 'mtg-cube-dev',
-    storageBucket: 'mtg-cube-dev.appspot.com',
-    messagingSenderId: '802482759052'
+    apiKey: 'AIzaSyDOt-nC9JsrgBVfocvGY7vgGWoB-WNoKoE',
+    authDomain: 'mtg-cube.firebaseapp.com',
+    databaseURL: 'https://mtg-cube.firebaseio.com',
+    projectId: 'mtg-cube',
+    storageBucket: 'mtg-cube.appspot.com',
+    messagingSenderId: '600294721599'
   }
 };
